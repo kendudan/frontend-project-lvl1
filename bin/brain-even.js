f9e06302a159ffcecbf624992feff937cda375c3
kendudan <annadudek19920509@gmail.com>
2021-04-22 13:13:38 +0300

    добавлены правки

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -18,13 +18,18 @@ while (correctAnswer < 3) {
 		correctAnswer += 1;
 	} else if (userAnswer === 'yes' && showInt % 2 !== 0) {
 		console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${userName}!`);
+		break;
 	}else if (userAnswer === 'no' && showInt % 2 !== 0) {
 		console.log('Correct!');
 		correctAnswer += 1;
 	} else if (userAnswer === 'no' && showInt % 2 === 0) {
 		console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, ${userName}!`);
+		break;
 	} else {
-		console.log (`${userAnswer} is wrong answer ;(. \n Let's try again, ${userName}!`)
+		console.log (`${userAnswer} is wrong answer ;(. \n Let's try again, ${userName}!`);
+		break;
 	}
 }
-console.log(`Congratulations, ${userName}!`);
+if (correctAnswer === 3) {
+	console.log(`Congratulations, ${userName}!`);
+};
