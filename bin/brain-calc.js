f21fbe142a043918011fb0d083182ec48ce900be
kendudan <annadudek19920509@gmail.com>
2021-04-30 16:47:09 +0300

    Исправлены ошибки линтера

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,32 +1,48 @@
-import getRandomInt from '../src/randomInt.js';
+#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
+import getRandomInt from '../src/randomInt.js';
 import userName from '../src/cli.js';
 
 const mathOperationsArray = ['+', '-', '*'];
 
 const generateRandomMathOperation = (array) => {
-	const randomNum = Math.floor(Math.random() * array.length);
-	return array[randomNum];
-}
+  const randomNum = Math.floor(Math.random() * array.length);
+  return array[randomNum];
+};
 
 console.log('What is the result of the expression?');
 let correctAnswer = 0;
 while (correctAnswer < 3) {
-	const firstInt = getRandomInt(1, 50);
-	const secondInt = getRandomInt(1, 50);
-	const mathOperation = generateRandomMathOperation(mathOperationsArray);
+  const firstInt = getRandomInt(1, 50);
+  const secondInt = getRandomInt(1, 50);
+  const mathOperation = generateRandomMathOperation(mathOperationsArray);
 
-	console.log(`Question: ${firstInt} ${mathOperation} ${secondInt}`);
-	const userAnswer = readlineSync.question('Your answer: ');
-	const expression = `${firstInt} ${mathOperation} ${secondInt}`;
-	const answer = eval(expression);
+  console.log(`Question: ${firstInt} ${mathOperation} ${secondInt}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  let answer;
+  switch (mathOperation) {
+    case '+':
+      answer = firstInt + secondInt;
+      break;
+    case '-':
+      answer = firstInt - secondInt;
+      break;
+    case '*':
+      answer = firstInt * secondInt;
+      break;
+    default:
+      break;
+  }
+  console.log(typeof (answer));
+  console.log(typeof (userAnswer));
 
-	if (userAnswer == answer) {
-		console.log('Correct');
-		correctAnswer += 1;
-	} else if (userAnswer != answer) {
-		console.log(`${userAnswer} is wrong answer ;(. Correct answer was '${answer}'.\n Let's try again, ${userName}!`);
-	}
+  if (userAnswer === answer.toString()) {
+    console.log('Correct');
+    correctAnswer += 1;
+  } else if (userAnswer !== answer.toString()) {
+    console.log(`${userAnswer} is wrong answer ;(. Correct answer was '${answer}'.\n Let's try again, ${userName}!`);
+  }
 }
 if (correctAnswer === 3) {
   console.log(`Congratulations, ${userName}!`);
